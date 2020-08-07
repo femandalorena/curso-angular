24f2c3daf1f92e01b68ee0cc30c898bfb44fb21f
femandamartinez <f.l.m.m.0027@gmail.com>
2020-08-06 23:05:12 -0300

    my first observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
+import {from, fromEvent} from 'rxjs';
+  
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'curso-angular';
+
+  ngOnInit () {
+    const array = from ([1,2,3,4,5,6]);
+    array.subscribe (a => console.log('item: ', a) )
+    const aux = fromEvent (document, 'mousemove');
+    aux.subscribe((a:any) => console.log('event: ' + a.clientX + ', ' + a.clientY));
+  }
 }
