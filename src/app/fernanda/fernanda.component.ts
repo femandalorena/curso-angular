d06303a1b758ae3e895c36b9e7f1c2f531b9e66f
femandamartinez <f.l.m.m.0027@gmail.com>
2020-08-06 23:47:50 -0300

    my firsts components first try

---
+++ src/app/fernanda/fernanda.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fernanda',
+  templateUrl: './fernanda.component.html',
+  styleUrls: ['./fernanda.component.scss']
+})
+export class FernandaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/lorena/lorena.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lorena',
+  templateUrl: './lorena.component.html',
+  styleUrls: ['./lorena.component.scss']
+})
+export class LorenaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
