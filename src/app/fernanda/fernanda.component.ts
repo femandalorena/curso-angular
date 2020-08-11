4e1bb7c066efff90d0a0575954294020ad9ea558
femandamartinez <f.l.m.m.0027@gmail.com>
2020-08-11 00:08:26 -0300

    commit monday 8/11/2020

--- src/app/fernanda/fernanda.component.ts
+++ src/app/fernanda/fernanda.component.ts
@@ -1,15 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  DoCheck,
+  OnChanges,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+} from "@angular/core";
 
 @Component({
-  selector: 'app-fernanda',
-  templateUrl: './fernanda.component.html',
-  styleUrls: ['./fernanda.component.scss']
+  selector: "person",
+  template: `
+    <div style="border: 1px solid red;">
+    <p>FER</p>
+    <input type="text" [(ngModel)]="name">
+    <p>VALOR VAR NAME: {{name}}</p>
+    </div>
+  `,
+  styles: [
+    `
+    
+    `
+  ]
 })
-export class FernandaComponent implements OnInit {
 
-  constructor() { }
+export class fernandaComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy {
+  @Input() name: string;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit() {
+    console.log("ON INIT");
+  }
+
+  ngOnChanges() {
+    console.log("ON CHANGES");
+  }
+
+  ngDoCheck() {
+    console.log("DO CHECK");
+  }
+
+  ngAfterContentInit() {
+    console.log("AfterContentInit");
+  }
+
+  ngAfterContentChecked() {
+    console.log("AfterContentChecked");
+  }
+
+  ngAfterViewInit() {
+    console.log("AfterViewInit");
+  }
+
+  ngAfterViewChecked() {
+    console.log("AfterViewChecked");
+  }
+
+  ngOnDestroy(){
+    console.log("OnDestroy");
+
   }
 
 }
