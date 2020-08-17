ab97d7b4b6720f2dc538c8b976c5f7e9f19af6f8
femandamartinez <f.l.m.m.0027@gmail.com>
2020-08-17 16:55:40 -0300

    directive change div color

--- src/app/directives/ng-color.directive.ts
+++ src/app/directives/ng-color.directive.ts
@@ -1,11 +1,27 @@
-import { Directive, ElementRef } from "@angular/core";
+import { Directive, ElementRef, HostListener, Input, Output } from "@angular/core";
 
 @Directive({
   selector: '[appNgColor]'
 })
 export class NgColorDirective {
-  constructor (el:ElementRef){
-    console.log('Element ref:' , el)
-    el.nativeElement.style.backgroundColor = 'yellow';
+   @HostListener('mouseenter') onMouseEnter(){
+    console.log('MOUSE ENTER')
+    this.el.nativeElement.style.backgroundColor = 'yellow'; 
   }
+
+  @HostListener('mouseleave') onMousdeLeave(){
+    console.log('MOUSE LEAVE')
+    this.changeColor('red');
+  }
+
+  constructor(private el: ElementRef) { 
+    console.log('ELEMENT REF: ', el);
+    this.changeColor('yellow');
+  }
+
+
+  changeColor(color:string){
+    this.el.nativeElement.style.backgroundColor = color; 
+  }  
+
 }
