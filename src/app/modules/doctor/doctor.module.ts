88cc565c56384cc59b9c7fbb526781d49ea3d00f
femandamartinez <f.l.m.m.0027@gmail.com>
2020-08-20 21:09:54 -0300

    modules homework but i still have questions about owner 2 being declarated twice

--- src/app/modules/doctor/doctor.module.ts
+++ src/app/modules/doctor/doctor.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Doctor1Component } from './doctor1/doctor1.component';
 import { Owner2Component } from '../owner/owner2/owner2.component';
+import { OwnerModule } from '../owner/owner.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    OwnerModule
   ],
   declarations: [
-    Doctor1Component,
-    Owner2Component
+    Doctor1Component
   ]
 })
 export class DoctorModule { }
