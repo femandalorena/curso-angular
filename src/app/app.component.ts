2a352627cc783ca92d0f4dc8ce446184030aa8f8
femandamartinez <f.l.m.m.0027@gmail.com>
2020-08-25 17:15:12 -0300

    first commit: pedidos ya page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,83 +1,14 @@
-import { Component, OnInit, Input} from '@angular/core';
-import {from, fromEvent} from 'rxjs';
-  
+import {Component, OnInit} from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent implements OnInit{
-  title = 'curso-angular';
-  name= "alguien";
-  lastname= "pues";
-  numbers = [1,2,3,4,5,6,7,8,9,10]
-  persons = [{
-      name: 'martina',
-      lastName: 'fernandez',
-      age: 11,
-      enable: false},{
-      name: 'marco',
-      lastName: 'martinez',
-      age: 12,
-      enable: false},{
-      name: 'maria',
-      lastName: 'marcos',
-      age: 13,
-      enable: true},{
-      name: 'marta',
-      lastName: 'martins',
-      age: 14,
-      enable: true
-    }]
-  animals = [{nombre: "choco", color: "cafe", edad: 2, vacunado: true},
-{nombre: "perla", color: "amarillo", edad: 4 , vacunado: false},
-{nombre: "jorge", color: "cafe", edad: 5, vacunado: false},
-{nombre: "beto", color: "negro", edad: 5 , vacunado: true},
-{nombre: "berni", color: "negro", edad: 4, vacunado: true},
-{nombre: "cuki", color: "negro", edad: 7 , vacunado: false},
-{nombre: "lobo", color: "amarillo", edad: 1 , vacunado: false},
-{nombre: "bozer", color: "negro", edad: 4 , vacunado: true},
-{nombre: "dormilon", color: "cafe", edad: 4 , vacunado: false},
-{nombre: "vandido", color: "café", edad: 5 , vacunado: false}
-]
+export class AppComponent implements OnInit {
 
-vacunados= this.animals.filter(s => s.vacunado===true)
-novacunados=this.animals.filter(s => s.vacunado ===false)
-
-
-
-  sw = true;
-
-  personas = ['mar', 'maria', 'marco', 'marina']
-  aux = 6
-  auxColor = 'blue';
-  auxClass = true;
-  
-  ngOnInit () {
-    const array = from ([1,2,3,4,5,6]);
-    array.subscribe (a => console.log('item: ', a) )
-    const aux = fromEvent (document, 'mousemove');
- //   aux.subscribe((a:any) => console.log('event: ' + a.clientX + ', ' + a.clientY));
-
-
+  ngOnInit(): void {
   }
-  pure (a:number, b:number){
-    return a+b;
-  }
-  impure (a:number, b:number){
-    return a+b+Math.random();
-  }
-  onClickSave(event){
-  }
-  vacunar (animal){
-    
-    ind= this.novacunados.findIndex(a => a === animal);
-    this.novacunados.splice(ind, 1);
-    this.vacunados.push(animal);
-  }
-  changeName(){
-    this.name='nadie';
-  
-  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,49 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { LorenaComponent } from './componets/lorena/lorena.component';
-import { PersonModule } from './modules/person/person.module';
-import { ProductModule } from './modules/product/product.module';
-import { AdminModule } from './modules/admin/admin.module';
-import { UserModule } from './modules/user/user.module';
-import { TestPipe } from './pipes/test.pipe';
-import { ExpoPipe } from './pipes/exponent.pipe';
-import { PurePipe } from './pipes/pure.pipe';
-import { ImpurePipe } from './pipes/impure.pipe';
-import { NgColorDirective } from './directives/ng-color.directive';
-import { AnimalsComponent } from './animals.component';
-import { HomeComponent } from './componets/home/home.component';
-import { AboutComponent } from './componets/about/about.component';
-import { AdminComponent } from './componets/admin/admin.component';
-import { AppRoutingModule } from './app.routing.module';
-import { Home1Component } from './componets/home1/home1.component';
-import { Home2Component } from './componets/home2/home2.component';
+import {AppRoutingModule} from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MatSliderModule} from '@angular/material/slider';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LorenaComponent, 
-    TestPipe,
-    ExpoPipe, 
-    PurePipe,
-    ImpurePipe,
-    NgColorDirective,
-    AnimalsComponent,
-     HomeComponent,
-    AboutComponent,
-    AdminComponent,
-    Home1Component,
-    Home2Component ],
+    AppComponent
+  ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
-    PersonModule,
-     ProductModule,
-     AdminModule,
-     UserModule, 
-     AppRoutingModule
+    AppRoutingModule,
+
+    MatSliderModule,
+    MatIconModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
