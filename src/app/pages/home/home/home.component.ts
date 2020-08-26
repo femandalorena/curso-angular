29979c91a11fcc517dc8bd3208686fa0192d1c7a
femandamartinez <f.l.m.m.0027@gmail.com>
2020-08-26 16:19:25 -0300

    working with angular material: first commit in class for wednesday

--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  products = [
+    {
+      description: 'ricas',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 4,
+      title: 'tucus'
+    },
+    {
+      description: 'deliciosas',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 2,
+      price: 5,
+      title: 'salteñas'
+    },
+    {
+      description: 'frutifantasticos',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 10,
+      title: 'licuados de papaya'
+    },
+    {
+      description: 'grasosos',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 2,
+      price: 6,
+      title: 'pasteles'
+    }
+  ];
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
 }
