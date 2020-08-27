e46fd3a58830eae2fefb5b2b091233ce1cf2c866
femandamartinez <f.l.m.m.0027@gmail.com>
2020-08-27 16:38:11 -0300

    first homework for today: ngelse

---
+++ src/app/directives/ng-else.directive.ts
@@ -0,0 +1,19 @@
+import {Directive, ElementRef, TemplateRef, ViewContainerRef, Input, OnInit} from '@angular/core';
+@Directive({
+  selector: '[ngElse]'
+})
+export class NgElseDirective implements OnInit {
+  @Input()
+  set ngElse(condition: boolean){
+    if (condition){
+      this.viewContainer.clear();
+    } else{
+      this.viewContainer.createEmbeddedView (this.templateRef);
+    }
+  }
+  constructor (private templateRef: TemplateRef <any>, 
+    private viewContainer: ViewContainerRef){}
+    ngOnInit(){
+
+    }
+}
