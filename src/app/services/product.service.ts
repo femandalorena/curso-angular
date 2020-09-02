d88ac58c1144629870dbc0a2d17b0ee8b169460a
femandamartinez <f.l.m.m.0027@gmail.com>
2020-09-02 16:16:02 -0300

    commit for post

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,7 +3,6 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
 @Injectable()
-
 export class ProductService {
 
   constructor(private http: HttpClient) {
@@ -13,5 +12,10 @@ export class ProductService {
   public getProducts(): Observable<any> {
     return this.http.get('https://angular-course-bcdc9.firebaseio.com/products.json');
   }
+
+  public addProduct(product: any): Observable<any> {
+    return this.http.post('https://angular-course-bcdc9.firebaseio.com/products.json', product);
+  }
+
 
 }
