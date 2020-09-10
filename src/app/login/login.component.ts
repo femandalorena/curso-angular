e7154d88fcbefe606327c3acfc6a1d64eadf80fd
femandamartinez <f.l.m.m.0027@gmail.com>
2020-09-10 17:27:04 -0300

    auth guard

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -19,9 +19,14 @@ export class LoginComponent implements OnInit {
   onLogin(form: any): void {
     console.log('FORM: ', form.value);
 
-    this.authService.login(form.value).subscribe(
+    this.authService.login({
+      email: form.value.email,
+      password: form.value.password,
+      returnSecureToken: true
+    }).subscribe(
       res => {
         console.log('LOGIN RESPONSE: ', res);
+        this.router.navigate(['pages']);
       },
       err => {
         console.log('LOGIN ERROR: ');
