ae624a4a5c5d90dffa9e1db9a7c2c3f833196537
femandamartinez <f.l.m.m.0027@gmail.com>
2020-09-17 19:11:01 -0300

    2 files modified, 1 added

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -1,6 +1,6 @@
 import {ActionReducerMap} from '@ngrx/store';
 
-import * as fromHome from '../admin/student/store/home.reducer';
+import * as fromHome from '../admin/student/store/student.reducer';
 
 export const reducers: ActionReducerMap<any> = {
   home: fromHome.reducer
