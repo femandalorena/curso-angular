1dcdce04b0d73299dae8dcf3ee9a55c03fc70931
femandamartinez <f.l.m.m.0027@gmail.com>
2020-09-17 19:13:43 -0300

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
   {path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule)},
   {
     path: 'admin',
-    loadChildren: () => import('./admin/admin.module').then(m => m.PagesModule),
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
     canActivate: [AuthGuard]
   }
 ];
